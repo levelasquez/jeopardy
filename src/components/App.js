d081fb0062975d4336edf865ed915fffa622c520
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-16 17:02:58 -0300

    connecting the app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { setCategories } from '../actions'
 
 class App extends Component {
   componentDidMount() {
-    fetch('http://jservice.io/api/categories?count=20')
-      .then(response => response.json())
-      .then(json => console.log(json))
+    const { categories, setCategories } = this.props
+
+    if (categories.length === 0) {
+      fetch('http://jservice.io/api/categories?count=20')
+        .then(response => response.json())
+        .then(json => setCategories(json))
+    }
   }
 
   render() {
+    const { categories } = this.props
+    console.log({ categories })
+
     return (
       <div>
         <h2>Jeopardy</h2>
@@ -16,4 +25,8 @@ class App extends Component {
   }
 }
 
-export default App
+const mapStateToProps = state => ({ categories: state })
+
+const connectedApp = connect(mapStateToProps, { setCategories })(App)
+
+export default connectedApp
