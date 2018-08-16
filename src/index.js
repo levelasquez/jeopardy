fa515eac96d0b5fc4d44b5f0065733ab2327b899
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-16 17:09:57 -0300

    Adding routing and a Category Component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import rootReducer from './reducers'
 import App from './components/App'
+import Category from './components/Category'
 
 const store = createStore(rootReducer)
 
 const app = (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/category' component={Category} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
 )
 
