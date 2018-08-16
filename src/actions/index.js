e79f1688802282466a3407bafb0a9cdfde239e9b
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-16 16:54:36 -0300

    setting categories through redux

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const SET_CATEGORIES = 'SET_CATEGORIES'
+
+const setCategories = categories => ({
+  type: SET_CATEGORIES,
+  categories,
+})
+
+export {
+  SET_CATEGORIES,
+  setCategories,
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(rootReducer)
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
